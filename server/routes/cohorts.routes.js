2c6e787200db2ecf6ca3f91aa1937e4f13b12b23
Karl97490 <karl.pery@icloud.com>
2026-06-18 21:22:16 +0400

    destructuring obj body in API routes

--- server/routes/cohorts.routes.js
+++ server/routes/cohorts.routes.js
@@ -1,32 +1,35 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // IMPORT MODEL
 const Cohort = require("../models/Cohorts.model");
 
-function errorFunction () {
-    return Promise.reject('Promise rejected')
-} 
+function errorFunction() {
+  return Promise.reject("Promise rejected");
+}
 
-router.get("/", async (req, res, next) => { // returns all cohorts from the database.
+router.get("/", async (req, res, next) => {
+  // returns all cohorts from the database.
   try {
-    const response = await Cohort.find(); 
-    res.json(response); 
+    const response = await Cohort.find();
+    res.json(response);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
-router.get("/:cohortId", async (req, res, next) => { // returns a specific cohort using its id.
+router.get("/:cohortId", async (req, res, next) => {
+  // returns a specific cohort using its id.
   try {
     const response = await Cohort.findById(req.params.cohortId);
     res.json(response);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
-router.post("/", async (req, res, next) => { // creates a new cohort document.
+router.post("/", async (req, res, next) => {
+  // creates a new cohort document.
   const {
     cohortSlug,
     cohortName,
@@ -57,31 +60,58 @@ router.post("/", async (req, res, next) => { // creates a new cohort document.
     const response = await Cohort.create(newCohort);
     res.json(response);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
-router.put("/:cohortId", async (req, res, next) => { // updates an existing cohort.
+router.put("/:cohortId", async (req, res, next) => {
+  // updates a cohort
+  const {
+    cohortSlug,
+    cohortName,
+    program,
+    format,
+    campus,
+    startDate,
+    endDate,
+    inProgress,
+    programManager,
+    leadTeacher,
+    totalHours,
+  } = req.body;
   try {
+    const updatedCohort = {
+      cohortSlug,
+      cohortName,
+      program,
+      format,
+      campus,
+      startDate,
+      endDate,
+      inProgress,
+      programManager,
+      leadTeacher,
+      totalHours,
+    };
     const response = await Cohort.findByIdAndUpdate(
       req.params.cohortId,
-      req.body,
+      updatedCohort,
       { returnDocument: "after", runValidators: true },
     );
     res.json(response);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
-router.delete("/:cohortId", async (req, res, next) => { // removes a cohort from the database.
+router.delete("/:cohortId", async (req, res, next) => {
+  // removes a cohort from the database.
   try {
     await Cohort.findByIdAndDelete(req.params.cohortId);
     res.json({ msg: "Deleted successfully" });
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
-
-module.exports = router
+module.exports = router;
